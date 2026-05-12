43387a331419128ab18d9024b85d045710704414
GabiDourado <gabi.dourados2xd@gmail.com>
2026-05-11 21:19:31 -0300

    adição das informaçõpes da tela de créditos - falta estilizar - precisa criar navegação para separar referencias e creditos - falta tela de referencias

--- lib/creditos.dart
+++ lib/creditos.dart
@@ -7,7 +7,64 @@ class Creditos extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Creditos')),
       body: Center(
-        child: Text('Creditos e referencias'),
+        child: Column(
+          children: [
+            const Text(
+              "Disciplina:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              "Desenvolvimento de software",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Professor:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              "Prof. Dr. Elvio Gilberto da Silva",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Equipe:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              "Eduardo Merlin Simão",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Gabriela Dos Santos Dourado",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Leridia Casanova Abrantes",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Curso:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Image.asset('assets/Ciencia_da_Computacao.jpg', height: 80),
+            const SizedBox(height: 15),
+            const Text(
+              "Apoio:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Image.asset('assets/extensao.jpg', height: 80),
+            const SizedBox(height: 15),
+          ],
+        ),
       ),
     );
   }
